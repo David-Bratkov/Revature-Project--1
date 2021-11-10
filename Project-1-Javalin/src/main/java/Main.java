75c4179d48b4b1b118d0d639ce548857b805cd02
David-Bratkov <bratkovdavid09@gmail.com>
2021-11-10 12:09:30 -0800

    Should work now hopefully plz dont break again

--- Project-1-Javalin/src/main/java/Main.java
+++ Project-1-Javalin/src/main/java/Main.java
@@ -1,2 +1,50 @@
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/", Location.CLASSPATH);
+        }).start(9000);
+
+        app.post("/form-add", context -> {
+
+            Double num1 = Double.parseDouble(context.formParam("add1"));
+            Double num2 = Double.parseDouble(context.formParam("add2"));
+
+            context.result("" + (num1 + num2));
+
+        });
+
+        app.post("/form-sub", context -> {
+
+            Double num1 = Double.parseDouble(context.formParam("sub1"));
+            Double num2 = Double.parseDouble(context.formParam("sub2"));
+
+            context.result("" + (num1 - num2));
+
+        });
+
+        app.post("/form-mult", context -> {
+
+            Double num1 = Double.parseDouble(context.formParam("mult1"));
+            Double num2 = Double.parseDouble(context.formParam("mult2"));
+
+            context.result("" + (num1 * num2));
+
+        });
+
+        app.post("/form-div", context -> {
+
+            Double num1 = Double.parseDouble(context.formParam("div1"));
+            Double num2 = Double.parseDouble(context.formParam("div2"));
+
+            context.result("" + (num1 / num2));
+
+        });
+
+
+
+
+    }
 }
